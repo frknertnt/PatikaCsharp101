c926f4f18c6eff8a52fcb4a8c0022320356d2f1a
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-12 15:58:44 +0300

    console

---
+++ StaticClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        // Check if a string is a palindrome.
+        string text1 = "racecar";
+        string text2 = "hello";
+
+        bool isPalindrome1 = Palindrome.IsPalindrome(text1);
+        bool isPalindrome2 = Palindrome.IsPalindrome(text2);
+
+        Console.WriteLine($"{text1} is a palindrome: {isPalindrome1}");
+        Console.WriteLine($"{text2} is a palindrome: {isPalindrome2}");
+    }
+}
