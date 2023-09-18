1ab1de4f15a15a7b31c11b5477951a0a98c3fc6a
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-18 16:22:36 +0300

    some book operation for crud

---
+++ UsingModel/WebApi/Application/BookOperations/GetBookDetail/GetBookDetailQuery.cs
@@ -0,0 +1,37 @@
+﻿using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.Application.BookOperations.GetBookDetail
+{
+    public class GetBookDetailQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public int BookId { get; set; }
+        public GetBookDetailQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public BookDetailViewModel Handle()
+        {
+            var book = _dbContext.Books.Where(b=> b.Id == BookId).SingleOrDefault();
+            if(book is null)
+            {
+                throw new InvalidOperationException("Book not found");
+            }
+            BookDetailViewModel viewModel = new BookDetailViewModel();
+            viewModel.Title = book.Title;
+            viewModel.PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy");
+            viewModel.Genre = ((GenreEnum)book.GenreId).ToString();
+            return viewModel;
+        }
+
+    }
+
+    public class BookDetailViewModel
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int PageCount { get; set; }
+        public string PublishDate { get; set; }
+    }
+}
---
+++ UsingModel/WebApi/Application/BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,40 @@
+﻿using WebApi.Common;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.BookOperations.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+
+        public GetBooksQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<BookViewModel> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(b=>b.Id).ToList<Book>();
+            List<BookViewModel> viewModel = new List<BookViewModel>();
+            foreach (var book in bookList)
+            {
+                viewModel.Add(new BookViewModel()
+                {
+                    Title = book.Title,
+                    Genre = ((GenreEnum)book.GenreId).ToString(),
+                    PublisDate = book.PublishDate.Date.ToString("dd/MM/yyy"),
+                    PageCount = book.PageCount, 
+                });
+            }
+            return viewModel;
+        }
+    }
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+        public string PublisDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
